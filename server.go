e07de2e0be534340789cad43f197a905490d2e6e
Vison Li <vison.li@verizonmedia.com>
2020-01-18 16:20:39 +0800

    update server

--- server.go
+++ server.go
@@ -14,37 +14,50 @@ type MyResponse struct {
 	Name string `json:"name,omitempty"`
 }
 
-func pingHandler() {
+type geometry interface {
+	area()
+	perim() float64
+}
 
+type rect struct{}
+
+func (this rect) area() {
+	return
+}
+
+type MyHandler struct {
+}
+
+func (this MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get name & get date
+	date := time.Now().Format("01/02/2006 15:04:05")
+	name := getNameFromQuery(r)
+
+	// generate response
+	s := MyResponse{
+		Text: "Hello World",
+		Date: date,
+		Name: name,
+	}
+
+	// write response
+	jsonString, err := json.Marshal(s)
+	_ = err
+	fmt.Fprintf(w, string(jsonString))
+
+	// logging
+	fmt.Printf("[%s] %s %s %s\n", date, r.Host, r.URL.Path, r.URL.RawQuery)
 }
 
 func main() {
-	t := time.Now()
+	server := http.Server{
+		Addr:        ":8080",
+		Handler:     MyHandler{},
+		ReadTimeout: 3 * time.Second,
+	}
 
-	fmt.Printf("[%s] Start http server.\n", t.Format("01/02/2006 15:04:05"))
-
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		// get name & get date
-		date := time.Now().Format("01/02/2006 15:04:05")
-		name := getNameFromQuery(r)
-
-		// generate response
-		s := MyResponse{
-			Text: "Hello World",
-			Date: date,
-			Name: name,
-		}
-
-		// write response
-		jsonString, err := json.Marshal(s)
-		_ = err
-		fmt.Fprintf(w, string(jsonString))
-
-		// logging
-		fmt.Printf("[%s] %s %s %s\n", date, r.Host, r.URL.Path, r.URL.RawQuery)
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("[%s] Start http server.\n", time.Now().Format("01/02/2006 15:04:05"))
+	log.Fatal(server.ListenAndServe())
 }
 
 func getNameFromQuery(r *http.Request) string {
