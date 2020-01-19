1ee17380af7096c91146215b0ea6a2fb3a3d034b
Vison Li <vison.li@verizonmedia.com>
2020-01-19 12:22:36 +0800

    test-a-successful-job

--- health-check.sh
+++ health-check.sh
@@ -1,7 +1,7 @@
 HTTP_CODE=$(curl -s -o /tmp/server.log -w %{http_code} "http://localhost:80/health-check")
 
 if [ $HTTP_CODE -eq 200 ]; then
-  echo "ok"
+  echo "okok"
   exit 0
 else 
   echo "fail"
